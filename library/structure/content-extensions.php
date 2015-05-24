ee4e7bc0c3c210bde360d20b72d2e95fe35c7628
Dennis Kieselhorst <mail@dekies.de>
2015-05-24 15:39:41 +0200

    Add thumbnails to single post view in Travelify theme
    added code from https://gist.github.com/puikinsh/8420137

--- library/structure/content-extensions.php
+++ library/structure/content-extensions.php
@@ -260,6 +260,19 @@ function travelify_theloop_for_single() {
 
 			<?php do_action( 'travelify_before_post_header' ); ?>
 
+	            <?php
+	            if( has_post_thumbnail() ) {
+	                $image = '';                    
+	                $title_attribute = apply_filters( 'the_title', get_the_title( $post->ID ) );
+	                $image .= '<figure class="post-featured-image">';
+	                $image .= '<a href="' . get_permalink() . '" title="'.the_title( '', '', false ).'">';
+	                $image .= get_the_post_thumbnail( $post->ID, 'featured', array( 'title' => esc_attr( $title_attribute ), 'alt' => esc_attr( $title_attribute ) ) ).'</a>';
+	                $image .= '</figure>';
+	 
+	                echo $image;
+	            }
+	            ?>
+
 			<header class="entry-header">
     			<h2 class="entry-title">
     				<?php the_title(); ?>
